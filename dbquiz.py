9de52c4fa1d4ee5a5ce4b9ff76ff2e3f010bf897
내he0o0nje <dudwogjswp@naver.com>
2023-07-12 20:51:07 +0900

    7-12

--- dbquiz.py
+++ dbquiz.py
@@ -4,4 +4,17 @@ db = client.dbsparta
 
 ## 코딩 할 준비 ##
 
+# target_movie = db.movies.find_one({'title':'매트릭스'})
+# print (target_movie['star'])
+
+
+# target_movie = db.movies.find_one({'title':'매트릭스'})
+# target_star = target_movie['star']
+#
+# movies = list(db.movies.find({'star':target_star}))
+#
+# for movie in movies:
+#     print(movie['title'])
+
+
 db.movies.update_one({'title':'매트릭스'},{'$set':{'star':'0'}})
